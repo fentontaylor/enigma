26de0b7db95cebb9ee387a02ec61e15f16b44269
Fenton Taylor <fentontaylor@gmail.com>
2019-07-26 16:44:50 -0600

    Refactor Key.get_keys to allow user to input desired key

--- lib/key.rb
+++ lib/key.rb
@@ -1,13 +1,12 @@
 class Key
   include RandomChars
 
-  def get_keys
-    keygen = random_digits(5)
+  def get_keys(key = random_digits(5))
     {
-      A: keygen[0..1],
-      B: keygen[1..2],
-      C: keygen[2..3],
-      D: keygen[3..4]
-    }.transform_values { |val| val.to_i}
+      A: key[0..1],
+      B: key[1..2],
+      C: key[2..3],
+      D: key[3..4]
+    }.transform_values { |val| val.to_i }
   end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -9,9 +9,15 @@ class KeyTest < Minitest::Test
     assert_instance_of Key, @key
   end
 
-  def test_get_keys
+  def test_get_keys_with_random_digits
     @key.stubs(:random_digits).returns('02715')
     expected = { A: 2, B: 27, C: 71, D: 15 }
     assert_equal expected, @key.get_keys
+  end
+
+  def test_get_keys_with_given_digits
+    keys = @key.get_keys('12345')
+    expected = { A: 12, B: 23, C: 34, D: 45 }
+    assert_equal expected, keys 
   end
 end
